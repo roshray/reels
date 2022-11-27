f31885a70c0ab2787b99182ca33dadb50dba5001
roshray <roshanroy759@gmail.com>
2022-11-27 23:29:54 +0530

    bug:use client id[missed] & username 2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['gravatar.com']
+  }
 }
 
 module.exports = nextConfig
