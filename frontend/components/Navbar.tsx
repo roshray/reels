ef14fe2606b9d108e06ef7e009c7f9a09020b29a
roshray <roshanroy759@gmail.com>
2022-11-29 18:41:23 +0530

    New Google oauth

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,16 +3,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { useRouter } from 'next/router'
-
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from '../utils'
 
 const Navbar = () => {
+  const user = false
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -25,6 +26,17 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+        <div>Search</div>
+        <div>
+          {user ? (
+            <div>Logged In</div>
+          ): (
+            <GoogleLogin
+              onSuccess={(response) => createOrGetUser(response)}
+              onError={() => console.log('Error')}
+            />
+          )}
+        </div>
     </div>
   )
 }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
 export default function App({ Component, pageProps }: AppProps) {
   const [isSSR, setIsSSR] = useState(true)
 
@@ -15,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   if(isSSR) return null
 
   return (
-    <div>
+     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -25,6 +27,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   )
 }
