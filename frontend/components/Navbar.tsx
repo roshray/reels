12bfe5c320caf9f16f2ba1034fc6b5b8dd7788ec
roshray <roshanroy759@gmail.com>
2022-12-01 23:20:06 +0530

    signIn userName

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -12,8 +12,12 @@ import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
 import { createOrGetUser } from '../utils'
 
+import useAuthStore from '../store/authStore'
+
 const Navbar = () => {
-  const user = false
+  const { userProfile, addUser} = useAuthStore()
+  
+  
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -28,11 +32,11 @@ const Navbar = () => {
         </Link>
         <div>Search</div>
         <div>
-          {user ? (
-            <div>Logged In</div>
+          {userProfile ? (
+            <div>{userProfile.userName}</div>
           ): (
             <GoogleLogin
-              onSuccess={(response) => createOrGetUser(response)}
+              onSuccess={(response) => createOrGetUser(response,addUser)}
               onError={() => console.log('Error')}
             />
           )}
