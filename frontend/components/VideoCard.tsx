2e59b9c42d83b77e3550def75e2e637cdc68ea75
roshray <roshanroy759@gmail.com>
2022-12-03 18:28:35 +0530

    video post on TL[sanity]: ☑

--- frontend/components/VideoCard.tsx
+++ frontend/components/VideoCard.tsx
@@ -73,7 +73,7 @@ const VideoCard: NextPage<IProps>= ({ post}) => {
                 className='rounded-3xl'>
                 <Link href="/">
                     <video
-                        className='lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+                        className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
                         loop
                         ref={videoRef}
                         src={post.video.asset.url}
